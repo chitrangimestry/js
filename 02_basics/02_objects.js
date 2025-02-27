929aa247ea5daf7e90be569aee2e7e4a843653fd
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2025-02-27 17:06:09 +0530

    Section 2 of JS

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,17 @@
+/*
+singleton
+object literals
+Object.create()
+*/
+
+
+const jsUser = {
+    name: "Max",
+    age: 20, 
+    location: Malvan,
+    email: "max@tester.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Thursday"]
+}
+
+console.log(jsUser[isLoggedIn])

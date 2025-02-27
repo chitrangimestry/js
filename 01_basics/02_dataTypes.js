59ecd39ee03f4405f2cee16f6994b2a8385c78bd
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2025-02-27 11:42:06 +0530

    operations in same file

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -30,8 +30,34 @@ let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
-
 let someNumber = 66;
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************04_Operations*******************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1 = "hello";
+let str2 = "  chitrangi";
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 22);
+// console.log(1 + 22 + "35");
+
+let gameCounter = 100;
+console.log(gameCounter)
+gameCounter++;
+console.log(gameCounter)
+++gameCounter;
+console.log(gameCounter)

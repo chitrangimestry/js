54ca159ed04f29942acbff1e61ee6183804d1047
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2025-02-27 11:18:21 +0530

    did datatype conversion in the same file

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,7 +2,7 @@
 
 // alert(3+3)
 
-console.log(typeof null);
+// console.log(typeof null);
 
 /*
 STRING
@@ -14,3 +14,24 @@ ARRAY
 NULL
 UNDEFINED
 */
+
+// Datatype conversion
+// 03_datatypes_conversion
+
+let score = null;
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 66;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

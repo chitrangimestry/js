c65b5fc39d02c4d102e982593534c1f68120a8ff
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2025-03-05 12:11:57 +0530

    modify files

--- 03_basics/03_functions.js
+++ 03_basics/03_functions.js
@@ -21,19 +21,4 @@ for (let i = 1; i <= 10; i++) {
   console.log(`No ${i} : This is Chrome Developers Tools`);
 }
 
-
-
-for (let i = 1; i < 10; i++) {
-  console.log(`Outer loop value: ${i}`);
-  for (let j = 1; j <= 10; j++) {
-    console.log(i + " * " + j + " = " + i * j);
-  }
-}*/
-
-for (let i = 0; i <= 20; i++) {
-  if (i == 5) {
-    console.log(`Detected 5`);
-    continue;
-  }
-  console.log(`Value of i is ${i}`);
-}
+*/
